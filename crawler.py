c07df3b74c2d860d9a4c82ebaca967f9d6f0ff55
shadyzoz <shadyzoz@163.com>
2017-02-13 19:09:18 +0800

    added soup

--- crawler.py
+++ crawler.py
@@ -1,4 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 r = requests.get('https://www.youtube.com/playlist?list=PLU6BYY1Lu_feVbuZEscpd6xT32zCrVrev')
-print(r.content)
+
+soup = BeautifulSoup(r.content)
+
+links = soup.tbody.find_all('a')
+
+for link in links:
+    print(link['href'])

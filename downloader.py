48530735e90fb1919503a7bcb74fcfbdec1f76a3
shadyzoz <shadyzoz@163.com>
2017-02-14 09:46:52 +0800

    fix filename

--- downloader.py
+++ downloader.py
@@ -5,7 +5,8 @@ from pytube import YouTube
 
 def download_video(url):
     yt = YouTube(url)
-    filename = yt.filename
+    filename = yt.filename.split(' - ')[1].split('Inside the NBA')[0].strip().replace(' ', '_')
+    yt.set_filename(filename)
     video = yt.get('mp4', '720p')
     video.download(os.path.join(os.getcwd(), '/videos/'))
     return filename

a0556b90140ec4921cecca683128e5e4899cbb14
shadyzoz <shadyzoz@163.com>
2017-02-14 14:47:03 +0800

    fix bug

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ import pafy
 def download_video(url):
     video = pafy.new(url)
     best = video.getbest()
-    title = best.title.split(':')[1].split('|')[0].strip().replace(' ', '_')
+    title = best.title.split(':')[1].split('|')[0].strip().replace('/', '').replace(' ', '_')
     filename = '{0}.{1}'.format(title, best.extension)
     best.download(os.path.join(os.getcwd(), 'videos', filename))
     return filename

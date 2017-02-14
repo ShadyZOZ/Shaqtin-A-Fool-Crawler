9ba993c690b9ac7bbcb21f0ce945b5399f0f73f9
shadyzoz <shadyzoz@163.com>
2017-02-14 13:10:59 +0800

    update

--- downloader.py
+++ downloader.py
@@ -7,4 +7,4 @@ def download_video(url):
     video = pafy.new(url)
     best = video.getbest()
     best.download(os.path.join(os.getcwd(), 'videos'))
-    return best.title
+    return '{0}.{1}'.format(best.title, best.extension)
